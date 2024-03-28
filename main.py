cc200d5a80197adf9e268dbc9e388046d42d9a17
Ariel Carmen <arielahognisse@gmail.com>
2024-03-28 13:04:31 +0100

    rating tables initialized

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import streamlit as st 
+import numpy as np
+
+ratings = np.array([
+    [1, 0, 3, 0, 0, 5, 0, 0, 5, 0, 4, 0],
+    [0, 0, 5, 4, 0, 0, 4, 0, 0, 2, 1, 3],
+    [2, 4, 0, 1, 2, 0, 3, 0, 4, 3, 5, 0],
+    [0, 2, 4, 0, 5, 0, 0, 4, 0, 0, 2, 0],
+    [0, 0, 4, 3, 4, 2, 4, 0, 0, 0, 2, 5],
+    [1, 0, 3, 0, 3, 0, 0, 2, 0, 0, 4, 0],
+])
 # Set the app title 
 st.title('My First Streamlit App') 
 # Add a welcome message 
