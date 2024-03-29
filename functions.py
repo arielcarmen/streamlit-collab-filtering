ba342bc69a091649910eb13c4a72a23d81a5bf41
Ariel Carmen <arielahognisse@gmail.com>
2024-03-29 12:22:25 +0100

    conversion to dataframe done

--- functions.py
+++ functions.py
@@ -2,33 +2,33 @@ import numpy as np
 import pandas as pd
 import math
 
-ratings = np.array([
-    [1, 0, 3, 0, 0, 5, 0, 0, 5, 0, 4, 0],
-    [0, 0, 5, 4, 0, 0, 4, 0, 0, 2, 1, 3],
-    [2, 4, 0, 1, 2, 0, 3, 0, 4, 3, 5, 0],
-    [0, 2, 4, 0, 5, 0, 0, 4, 0, 0, 2, 0],
-    [0, 0, 4, 3, 4, 2, 4, 0, 0, 0, 2, 5],
-    [1, 0, 3, 0, 3, 0, 0, 2, 0, 0, 4, 0],
-])
+# ratingsu = np.array([
+#     [1, 0, 3, 0, 0, 5, 0, 0, 5, 0, 4, 0],
+#     [0, 0, 5, 4, 0, 0, 4, 0, 0, 2, 1, 3],
+#     [2, 4, 0, 1, 2, 0, 3, 0, 4, 3, 5, 0],
+#     [0, 2, 4, 0, 5, 0, 0, 4, 0, 0, 2, 0],
+#     [0, 0, 4, 3, 4, 2, 4, 0, 0, 0, 2, 5],
+#     [1, 0, 3, 0, 3, 0, 0, 2, 0, 0, 4, 0],
+# ])
 
 
-def movie_mean(movie):
+def movie_mean(ratings, movie):
     return np.sum(ratings[movie])/np.count_nonzero(ratings[movie])
 
 
-def similarity(movie1, movie2, users_number):
+def similarity(ratings, movie1, movie2, users_number):
     ratings_movie1 = ratings[movie1]
     ratings_movie2 = ratings[movie2]
 
-    mean_movie_1 = movie_mean(movie1)
-    mean_movie_2 = movie_mean(movie2)
+    mean_movie_1 = movie_mean(ratings, movie1)
+    mean_movie_2 = movie_mean(ratings, movie2)
 
     users_means = []
     indexes = []
     temp1 = temp2 = 0
 
     for i in range(users_number):
-        if ratings_movie1[i] != 0 and ratings_movie2[i] != 0:
+        if ratings_movie1[i] != None and ratings_movie2[i] != None:
             indexes.append(i)
             users_means.append((ratings_movie1[i]-mean_movie_1) * (ratings_movie2[i]-mean_movie_2))
     
@@ -44,12 +44,12 @@ def similarity(movie1, movie2, users_number):
     return round(similarity_value, 2)
 
 
-def top_n_similarity(movie, n, users_number):
+def top_n_similarity(ratings, movie, n, users_number):
     similarities = []
     
     for i in range(len(ratings)):
         if i != movie:
-            sim = similarity(movie, i, users_number)
+            sim = similarity(ratings, movie, i, users_number)
             similarities.append((i, sim))
    
     similarities.sort(key=lambda x: x[1], reverse=True)
@@ -57,14 +57,14 @@ def top_n_similarity(movie, n, users_number):
     top_n = similarities[:n]
     return top_n
 
-def predict_rating(n, users_number, selected_movie, selected_user):
-    top_n_similarities = top_n_similarity(selected_movie, n, users_number)
+def predict_rating(ratings, n, users_number, selected_movie, selected_user):
+    top_n_similarities = top_n_similarity(ratings, selected_movie, n, users_number)
     rates_by_means = 0
 
     sims= []
     for item in top_n_similarities:
-        user_rating = ratings[item[0]][selected_user]
-        if user_rating != 0:
+        user_rating = ratings_table[item[0]][selected_user]
+        if user_rating != None:
             sim = similarity(selected_movie, item[0], users_number)
             rates_by_means += sim * user_rating
             sims.append(sim)
@@ -73,3 +73,5 @@ def predict_rating(n, users_number, selected_movie, selected_user):
 
 def array_to_dataframe(arr):
     return pd.DataFrame(arr)
+
+# predict_rating(2, 6, 1, 1)
--- webview.py
+++ webview.py
@@ -14,26 +14,41 @@ class_ratings = np.array([
     [1, 0, 3, 0, 3, 0, 0, 2, 0, 0, 4, 0],
 ])
 
+@st.cache_resource
+def generate_table():
+    return 
 column1, column2 = st.columns(2)
 
-movies_number = st.number_input('Nombre de films:', 1)
-users_number = st.number_input('Nombre d\'utilisateurs:', 1)
+# movies_number = st.number_input('Nombre de films:', 1)
+# users_number = st.number_input('Nombre d\'utilisateurs:', 1)
 top_number = st.number_input('N:', 2)
+scores_grid = pd.read_csv("movies.csv")
 
 if st.button(label= "Generer le tableau"):
-    scores_grid = arr = np.random.choice([0, 1, 2, 3, 4, 5], size=(movies_number, users_number), p=[0.5, 0.1, 0.1, 0.1, 0.1, 0.1])
-    st.dataframe(array_to_dataframe(scores_grid))
-        
+    
+    st.write(scores_grid)
+
 
 user_to_predict = st.number_input('Utilisateur dont on veut prédire la note:', 1)
 movie_to_predict = st.number_input('Film dont on veut prédire la note:', 1)
 if st.button(label= "Predire"):
+    print(scores_grid.count(axis=0).size)
     predicted_rating = predict_rating(
         n= top_number,
-        users_number= users_number,
+        users_number= scores_grid.count(axis=1).size,
         selected_movie= movie_to_predict - 1,
         selected_user= user_to_predict - 1
     )
     st.write(f"L'utilisateur {user_to_predict} pourrait donner au film {movie_to_predict}, un score de: {round(predicted_rating, 2)}")
 
-
+def generate_csv():
+    movies_labels = [f"Film {i}" for i in range (1, movies_number + 1)]
+    users_labels = [f"Utilisateur {i}" for i in range (1, users_number + 1)]
+    scores_grid = pd.DataFrame(np.random.choice([0, 1, 2, 3, 4, 5], size=(movies_number, users_number), p=[0.5, 0.1, 0.1, 0.1, 0.1, 0.1]))
+    scores_grid = scores_grid.replace(0, '')
+    scores_grid = scores_grid.set_index(pd.Index(movies_labels))
+    scores_grid = scores_grid.rename(columns = {i: users_labels[i] for i in range(users_number)})
+    
+    st.table(scores_grid)
+    scores_grid.to_csv('movies_.csv')
+        
