6b1ac1c512b676bb46cea9aaebd33e9c19c63770
arielcarmen <makandjou3000@gmail.com>
2024-04-27 21:00:29 +0100

    addind dataset

--- tp3/ahognisseariel.py
+++ tp3/ahognisseariel.py
@@ -28,69 +28,45 @@ if 'show_csv_field_' not in st.session_state:
 if 'choose_support_' not in st.session_state:
     st.session_state['choose_support_'] = False
 
-if 'point_predict_container_' not in st.session_state:
-    st.session_state['point_predict_container_'] = False
-
 if 'support_' not in st.session_state:
     st.session_state['support_'] = 2
 
+if 'itemset_' not in st.session_state:
+    st.session_state['itemset_'] = {}
 
 support = st.session_state['support_']
 datas_ = st.session_state['dataframe_']
+itemset_ = st.session_state['itemset_']
 
 # Definir la valeur de support
 def define_support():
     st.session_state['show_csv_field_'] = not st.session_state['show_csv_field_']
     st.session_state['choose_support_'] = not st.session_state['choose_support_']
-    st.session_state['dataset_size_'] = len(datas_)
+    
     return 0
 
-def apriori(transactions, min_support):
-    """
-    Implémentation de l'algorithme Apriori pour trouver les itemsets fréquents.
-    """
-    n = len(transactions)
-    item_counts = defaultdict(int)
+def generate_transactions(dataset):
+    transactions = []
+    for index, row in dataset.iterrows():
+        transactions.append(set(row.dropna().astype(str).tolist()))
+    return transactions
+
+def generate_itemset(transactions):
+    itemsets = set()
     for transaction in transactions:
         for item in transaction:
-            item_counts[item] += 1
+            itemsets.add(item)
+    return itemsets
 
-    L = [set([item]) for item, count in item_counts.items() if count >= min_support * n]
-
-    k = 2
-    while len(L[k - 2]) > 0:
-        C_k = apriori_gen(L[k - 2], k)
-        L_k = []
-        item_counts = defaultdict(int)
+# Fonction pour calculer le support des itemsets
+def calculate_support(transactions, itemsets, min_support):
+    support_data = {}
+    for itemset in itemsets:
         for transaction in transactions:
-            for c in C_k:
-                if set(c).issubset(transaction):
-                    item_counts[frozenset(c)] += 1
-        for itemset, count in item_counts.items():
-            if count >= min_support * n:
-                L_k.append(itemset)
-        L.append(L_k)
-        k += 1
-
-    return L
-
-def apriori_gen(L_k_minus_1, k):
-    """
-    Fonction pour générer les candidats pour l'étape suivante de l'algorithme Apriori.
-    """
-    C_k = []
-    for i in range(len(L_k_minus_1)):
-        for j in range(i + 1, len(L_k_minus_1)):
-            l1 = list(L_k_minus_1[i])
-            l2 = list(L_k_minus_1[j])
-            l1.sort()
-            l2.sort()
-            if l1[:k - 2] == l2[:k - 2]:
-                c = l1[:k - 2] + [max(l1[k - 2], l2[k - 2])] + [min(l1[k - 2], l2[k - 2])]
-                if not any(set(c).issubset(frozenset(x)) for x in L_k_minus_1):
-                    C_k.append(c)
-    return C_k
-
+            if itemset.issubset(transaction):
+                support_data[itemset] = support_data.get(itemset, 0) + 1
+    num_transactions = len(transactions)
+    return {item: support / num_transactions for item, support in support_data.items() if support / num_transactions >= min_support}
 
 # Déroulement du programme
 if st.session_state['show_csv_field_']:
@@ -98,7 +74,6 @@ if st.session_state['show_csv_field_']:
     if uploaded_file is not None:
         datas_ = pd.read_csv(uploaded_file)
         st.session_state['dataframe_'] = datas_
-        
         csv_loader.button("Valider", on_click= define_support)
     
 
@@ -109,6 +84,9 @@ if st.session_state['choose_support_'] == True:
     st.session_state['support_'] = support
 
     if  support_choice_.button("Lancer"):
-        aa = 0
+        transactions = generate_transactions(datas_)
+        itemsets = generate_itemset(transactions)
+        print(itemsets)
+        
 
         
